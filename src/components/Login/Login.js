cee7d87a7ec7b5ec31ea1546b8815d11fdd58143
Shivank <skj9519@gmail.com>
2022-02-16 13:19:15 +0530

    PART-10_3: Context

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useContext, useEffect, useReducer, useState } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT")
@@ -39,15 +40,17 @@ const Login = (props) => {
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
-	
+
+	const authCtx = useContext(AuthContext)
+
   useEffect(() => {
     const identifier = setTimeout(() => {
-      console.log("Checking");
+      // console.log("Checking");
       setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
 
     return () => {
-      console.log("cleanup");
+      // console.log("cleanup");
       clearTimeout(identifier);
     };
   }, [emailIsValid, passwordIsValid]);
@@ -74,7 +77,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
