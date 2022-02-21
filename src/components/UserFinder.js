0f97c58ad37b72a0100494bec24ef9d3af10bac9
Shivank <skj9519@gmail.com>
2022-02-21 12:55:31 +0530

    PART-13_4: Error Boundary in class based components only

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -4,6 +4,7 @@ import Users from "./Users";
 
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   // only one context allowed in class based component
@@ -42,7 +43,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
